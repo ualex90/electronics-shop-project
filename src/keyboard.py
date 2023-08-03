ae9695af5fce563f1e4efd9bad05af60f81a750c
u_alex90 <u_alex90@mail.ru>
2023-08-03 21:02:36 +1000

    mod keyboard doc

--- src/keyboard.py
+++ src/keyboard.py
@@ -6,7 +6,8 @@ class Keyboard(Item, MixinLang):
     def __init__(self, name_: str, price: float, quantity: int):
         super().__init__(name_, price, quantity)
         """
-        Создание экземпляра класса item.
+        Создание экземпляра класса item. Имеет свойства Item, 
+        дополнеными возможностью переключения языка клавиатуры из MixinLang
 
         :param name_: Название товара (в отличии от родительского класса Item, не имеет ограничения по длине имени)
         :param price: Цена за единицу товара.
