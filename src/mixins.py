2a6c77adcd3313232e499733ffb986ea3c806640
u_alex90 <u_alex90@mail.ru>
2023-08-03 20:31:03 +1000

    mod keyboard

--- src/mixins.py
+++ src/mixins.py
@@ -1,33 +1,15 @@
-class Node:
-    def __init__(self, lang):
-        self.lang = lang
-        self.change_lang = None
-
-    def __call__(self, *args, **kwargs):
-        return self.lang
-
-
 class MixinLang:
     def __init__(self) -> None:
-        self.__language = None
-        self.en = Node('EN')
-        self.ru = Node('RU')
-        self.en.change_lang = self.ru
-        self.ru.change_lang = self.en
+        self.__language = 'EN'
 
     @property
     def language(self):
         return self.__language
 
     def change_lang(self):
-        pass
-        # match self.__language:
-        #     case 'EN':
-        #         self.__language = 'RU'
-        #     case 'RU':
-        #         self.__language = 'EN'
-
-
-if __name__ == '__main__':
-    m = MixinLang()
-    m.change_lang()
+        match self.__language:
+            case 'EN':
+                self.__language = 'RU'
+            case 'RU':
+                self.__language = 'EN'
+        return self
--- tests/keyboard.py
+++ tests/keyboard.py
@@ -9,8 +9,8 @@ def test_change_lang(keyboard_1):
     assert str(keyboard_1.language) == "EN"
     keyboard_1.change_lang()
     assert str(keyboard_1.language) == "RU"
-    # keyboard_1.change_lang().change_lang()
-    # assert str(keyboard_1.language) == "RU"
+    keyboard_1.change_lang().change_lang()
+    assert str(keyboard_1.language) == "RU"
 
 
 def test_language(keyboard_1):
